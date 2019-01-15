f3c04bfc216abaffc02d85d0639f64888bd4fa2a
Toby HUANG <tecty@localhost.localdomain>
2019-01-15 11:12:16 +1100

    remove the redundent file

--- create_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# create an empty image 
-dd if=/dev/zero of=system_new.img bs=4k count=600
-# partition it as an ext4 disk
-mkfs.ext4 -F system_new.img
-
-# call the mount script to mount it 
-./mount
